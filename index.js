40f75c5117a0826737a65b38a231a98f61d394e4
Gitrupesh20 <rursharma02@gmail.com>
2024-06-26 12:16:36 +0530

    first commit

--- index.js
+++ index.js
@@ -20,9 +20,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  app.get('/api/:date?', (req, res) => {
+    let dateParam = req.params.date;
+  
+    let date;
+    if (!dateParam) {
+      date = new Date();
+    } else {
+      // Check if dateParam is a valid Unix timestamp or an ISO string
+      if (!isNaN(dateParam)) {
+        dateParam = parseInt(dateParam);
+      }
+      date = new Date(dateParam);
+    }
+  
+    // Check if the date is valid
+    if (date.toString() === 'Invalid Date') {
+      return res.json({ error: 'Invalid Date' });
+    }
+  
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  });
 
 
 
